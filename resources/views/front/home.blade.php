57d00e7c92697690ce6542d0f78b91cb9fc9af8b
kareem22T <m.othman@clicksegypt.net>
2024-10-10 18:28:05 +0300

    edits

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -685,9 +685,9 @@
             autoplay: true,
             loop: true,
             navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
 
             pagination: {
                 el: ".swiper-pagination",
