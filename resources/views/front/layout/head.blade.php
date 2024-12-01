be08a552c2c366a133c37763f150c8596cff1a9e
kareem22T <m.othman@clicksegypt.net>
2024-12-01 13:17:48 +0200

    edsit

--- resources/views/front/layout/head.blade.php
+++ resources/views/front/layout/head.blade.php
@@ -34,4 +34,16 @@
     </style>
     <title>{{ $settingsArray['site_name']['value'] }}</title>
     <link rel="shortcut icon" href="{{ asset('front/assets/imgs/favicon.png') }}" type="image/x-icon">
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HSTYYKVTH8"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'G-HSTYYKVTH8');
+        </script>
 </head>
